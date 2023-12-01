508998558c652810d096116db8e79b52829f3e16
SrorDekel <srordekel@gmail.com>
2023-12-01 13:52:19 -0300

    come on

--- db2_timeseries.c
+++ db2_timeseries.c
@@ -8,21 +8,6 @@
 
 #ifndef db2_internal_timeseries_h
 
-typedef struct 
-{
-    time_t _time;
-    unsigned _val_offset;
-    unsigned _val_size;
-} timeseries_entry_t;
-
-typedef struct
-{
-    db_value_t* _key;
-    unsigned _capacity;
-    unsigned _size;
-    timeseries_entry_t* _entries;
-} timeseries_t;
-
 #endif // db2_internal_timeseries_h
 
 static char timeseries_values[0x100000 * 20] = { 0 };

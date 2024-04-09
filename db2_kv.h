dc312b6eb97566c933bf40804d7aa141ddc483fd
DekelSror <srordekel@gmail.com>
2024-04-08 22:50:43 -0300

    db_value_t -> db2_value_t

--- db2_kv.h
+++ db2_kv.h
@@ -5,11 +5,11 @@
 
 #define db2_num_entries 0x10
 
-db_value_t* kv_find(struct db_op_find_t header);
+db2_value_t* kv_find(struct db_op_find_t header);
 int kv_remove(struct db_op_remove_t header);
 
 
-int kv_insert(uint64_t key_hash, db_value_t* key, db_value_t* val);
+int kv_insert(uint64_t key_hash, db2_value_t* key, db2_value_t* val);
 int kv_can_insert(struct db_op_insert_t header);
 
 #endif // db2_kv
